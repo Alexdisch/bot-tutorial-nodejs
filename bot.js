4bacb101ae0717b76fdcbb6043a422b1fc8efad8
Alexdisch <alex.disch@gmail.com>
2016-03-27 17:03:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ function respond() {
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://i.imgur.com/B5BSVqH.png");
+    postMessage("http://amazingyoungwomen.tumblr.com/image/116681965225");
     this.res.end();
   } 
   else if(request.text && botRegexAd.test(request.text)) {
