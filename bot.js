f9aa0a7f7b2ca991cd5733e930f30e6f36412a8e
Alexdisch <alex.disch@gmail.com>
2016-08-19 18:55:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexHarper = /^\/Harper/;botRegexSquirt = /^\/squirt/;botRegexBurn = /^\/burn/;botRegexTom = /^\/tom/;botRegexjj = /^\/jj/;botRegexRules = /^\/rules/;
-      botRegexAd=/^\/advance/;botRegexReid = /^\/Reid/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
+      botRegexAd=/^\/advance/;botRegexReid = /^\/reid/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
@@ -59,7 +59,7 @@ function respond() {
     postMessage("http://daddyleagues.com/PMC/rules");
     this.res.end();
   } 
-  else if(request.text && botRegexreid.test(request.text)) {
+  else if(request.text && botRegexReid.test(request.text)) {
     this.res.writeHead(200);
     postMessage("https://media4.giphy.com/media/g9RplpKZNkT5u/giphy.gif");
     this.res.end();
