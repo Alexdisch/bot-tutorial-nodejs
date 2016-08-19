c063793e743253be730a631a81315ade2c9f9135
Alexdisch <alex.disch@gmail.com>
2016-08-19 18:46:00 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexHarper = /^\/Harper/;botRegexSquirt = /^\/squirt/;botRegexBurn = /^\/burn/;botRegexTom = /^\/tom/;botRegexjj = /^\/jj/;botRegexRules = /^\/rules/;
-      botRegexAd=/^\/advance/;botRegexReid = /^\/reid/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
+      botRegexAd=/^\/advance/;botRegexReid = /^\/Reid/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
