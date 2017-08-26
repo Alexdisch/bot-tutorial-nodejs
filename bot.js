903d9a2c22571f0c4d46d304783ff7982e778475
Alexdisch <alex.disch@gmail.com>
2017-08-26 10:56:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,14 +75,14 @@ function respond() {
   } 
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/usba/players?name=&position=all&team="+request.text.substring(5,8));
-    //postMessage("http://daddyleagues.com/usba/team/"+request.text.substring(5,8)+"/depthchart");
+    postMessage("http://daddyleagues.com/ssfa/players?name=&position=all&team="+request.text.substring(5,8));
+    //postMessage("http://daddyleagues.com/ssfa/team/"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   
   else if(request.text && botRegexOW.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("www.daddyleagues.com/usba/maddenrating/");
+    postMessage("www.daddyleagues.com/ssfa/maddenrating/");
     this.res.end();
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
@@ -97,25 +97,25 @@ function respond() {
   }
   else if(request.text && botRegexRules.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://www.daddyleagues.com/usba/rules");
+    postMessage("https://www.daddyleagues.com/ssfa/rules");
     this.res.end();
   } 
   else if(request.text && botRegexTrades.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://www.daddyleagues.com/usba/forum");
+    postMessage("https://www.daddyleagues.com/ssfa/forum");
     this.res.end();
   } 
   else if(request.text && botRegexSC.test(request.text)) {
     this.res.writeHead(200);
     
-    postMessage("http://daddyleagues.com/usba/team/"+request.text.substring(5,8)+"/schedule");
+    postMessage("http://daddyleagues.com/ssfa/team/"+request.text.substring(5,8)+"/schedule");
     this.res.end();
   }
   else if(request.text && botRegexP.test(request.text)) {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
     var rep = req.replace(/ /,"+");
-    postMessage("http://daddyleagues.com/usba/players?name="+rep+"&position=all&team=all");
+    postMessage("http://daddyleagues.com/ssfa/players?name="+rep+"&position=all&team=all");
     
     this.res.end();
   }  
